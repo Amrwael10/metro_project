3bb1c0e4c803db7a5fef2dfb0f105a7aaaf8d535
Amrwael10 <116317398+Amrwael10@users.noreply.github.com>
2025-08-13 19:10:47 +0300

    Update HomePage.dart

--- lib/metro_project/screens/HomePage.dart
+++ lib/metro_project/screens/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:seconed_depi/metro_project/managers/ColorsManager.dart';
 import 'package:seconed_depi/metro_project/managers/RoutesManager.dart';
 
 class Homepage extends StatelessWidget {
@@ -8,22 +9,39 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Welcome to the Cairo metro line"),centerTitle: true,),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(onPressed: (){
-              Navigator.pushNamed(context, RoutesManager.metroline);
-            }, child: Text("choose from the metro lines")),
-            SizedBox(height: 10.h,),
-
-            ElevatedButton(onPressed: (){
-              Navigator.pushNamed(context, RoutesManager.locations);
-
-            }, child: Text("choose a location"))
-          ],
+      appBar: AppBar(
+        title: Text(
+            "Welcome to the Cairo metro line",
+            style: TextStyle(color: ColorsManager.purple),
         ),
+        centerTitle: true,),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/background.jpg"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/images/metrologo.png"),
+                const SizedBox(height: 14,),
+                ElevatedButton(onPressed: (){
+                  Navigator.pushNamed(context, RoutesManager.metroline);
+                }, child: Text("choose from the metro lines")),
+                SizedBox(height: 8.h,),
+                ElevatedButton(onPressed: (){
+                  Navigator.pushNamed(context, RoutesManager.locations);
+                }, child: Text("choose a location"))
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
