fc7fcdd8024fa25ea1678e2b917a22e7ec96cd41
Luka <161689534+KHALEDXHAWASH@users.noreply.github.com>
2025-08-15 03:16:27 +0300

    Delete lib directory
    يارب نخلص

--- lib/metro_project/managers/AssetsManager.dart
+++
@@ -1,7 +0,0 @@
-class AssetsManager
-{
-  static const String base = "assets/images";
-  static const String map = "$base/map.jpg";
-  static const String background = "$base/background.jpg";
-
-}
--- lib/metro_project/managers/ColorsManager.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:ui';
-
-class ColorsManager{
-  static const Color red = Color(0xFFC1272D);
-  static const Color gray = Color(0xFFBFBFBF);
-  static const Color black = Color(0xFF222222);
-  static const Color purple = Color(0xFF7B4F87);
-}
